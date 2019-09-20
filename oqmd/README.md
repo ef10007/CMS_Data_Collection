69678815a06b2a4e9331d074d8f4274b9a3d8d14
Han Jinny <jinseon-han@hotmail.com>
2019-09-20 15:08:37 +0900

    2019-9-20

--- oqmd/README.md
+++ oqmd/README.md
@@ -9,6 +9,7 @@ You can download the entire database with following url:
 
 ```wget http://www.oqmd.org/static/downloads/qmdb__v1_2__062018.sql.gz``` (8.9 GB) 
 
+
 #### Exploring database
 
 * Tables 
---
+++ oqmd/conn.py
@@ -0,0 +1,27 @@
+import os
+import requests
+from pprint import pprint
+import pymysql
+import json
+
+def get_conn():
+    return pymysql.connect(
+    host=os.getenv('oqmd_host'),
+    user=os.getenv('oqmd_user'),
+    password=os.getenv('oqmd_pw'),
+    port=3306,
+    db='oqdb',
+    charset='utf8')
+
+# print(os.getenv('oqmd_user'))
+
+sql_select = 'select * from atoms limit 10'
+
+conn = get_conn()
+    
+with conn:
+        
+    cur = conn.cursor()
+    cur.execute(sql_select)
+    rows = cur.fetchall()
+    print([row for row in rows])
